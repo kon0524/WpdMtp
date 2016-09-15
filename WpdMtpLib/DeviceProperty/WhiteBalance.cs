01c9f7379514a5dde74bf046129160a43aa37263
ntth <yasuhiro.nattoh@jrits.ricoh.co.jp>
2016-09-15 17:08:10 +0900

    MTP標準仕様のホワイトバランスの設定値を追加

--- WpdMtpLib/DeviceProperty/WhiteBalance.cs
+++ WpdMtpLib/DeviceProperty/WhiteBalance.cs
@@ -3,8 +3,13 @@ namespace WpdMtpLib.DeviceProperty
 {
     public enum WhiteBalance : ushort
     {
+        Undefined = 0x0000,
+        Manual = 0x0001,
         Automatic = 0x0002,
+        OnePushAutomatic = 0x0003,
         Daylight = 0x0004,
+        Florescent = 0x0005,
+        Flash = 0x0007,
         Shade = 0x8001,
         Cloudy = 0x8002,
         Tungsten1 = 0x0006,
