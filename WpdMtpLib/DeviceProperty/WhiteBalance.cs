8ff3792e5e2aea22387cc2eb80f64d10687ea427
ntth <yasuhiro.nattoh@jrits.ricoh.co.jp>
2016-09-26 15:06:12 +0900

    Merge commit 'fe21a335f2c8d3f0527cf8dd177d36e30e49a9c4'

--- WpdMtpLib/DeviceProperty/WhiteBalance.cs
+++ WpdMtpLib/DeviceProperty/WhiteBalance.cs
@@ -3,15 +3,20 @@ namespace WpdMtpLib.DeviceProperty
 {
     public enum WhiteBalance : ushort
     {
-        Automatic = 0x0002,
-        Daylight = 0x0004,
-        Shade = 0x8001,
-        Cloudy = 0x8002,
-        Tungsten1 = 0x0006,
-        Tungsten2 = 0x8020,
-        FluorescentLamp1_DaylightColor = 0x8003,
-        FluorescentLamp2_NeutralWhiteColor = 0x8004,
-        FluorescentLamp3_White = 0x8005,
-        FluorescentLamp4_LightBulbColor = 0x8006
+        Undefined                           = 0x0000,
+        Manual,
+        Automatic,
+        OnePushAutomatic,
+        Daylight,
+        Florescent,
+        Tungsten1,
+        Flash,
+        Shade                               = 0x8001,
+        Cloudy,
+        FluorescentLamp1_DaylightColor,
+        FluorescentLamp2_NeutralWhiteColor,
+        FluorescentLamp3_White,
+        FluorescentLamp4_LightBulbColor,
+        Tungsten2                           = 0x8020,
     }
 }
